9ce381d87b2d5a925c4cac7582e182786af1ae3c
nverno <noah.v.peart@gmail.com>
2016-10-17 06:31:33 -0400

    ye

--- Makefile
+++ Makefile
@@ -1,8 +1,18 @@
-cask ?= cask
 emacs ?= emacs
+wget ?= wget
 
 .PHONY: test
 all: test
 test:
 	$(emacs) -Q -batch -L . -l ert -l test/yaml-indent-tests.el \
 	-f ert-run-tests-batch-and-exit
+
+README.md : el2markdown.el yaml-indent.el
+	$(emacs) -batch -l $< yaml-indent.el -f el2markdown-write-readme
+
+.INTERMEDIATE: el2markdown.el
+el2markdown.el:
+	$(wget) -q -O $@ "https://github.com/Lindydancer/el2markdown/raw/master/el2markdown.el"
+
+clean:
+	$(RM) *~
--- yaml-indent.el
+++ yaml-indent.el
@@ -25,7 +25,20 @@
 
 ;;; Commentary:
 
-;; better indentation for yaml-mode
+;; [![Build Status](https://travis-ci.org/nverno/yaml-indent.svg?branch=master)](https://travis-ci.org/nverno/yaml-indent)
+
+;; Better indentation for `yaml-mode'.
+
+;; To use, set `indent-line-function' and `indent-region-function'
+;; to be `yaml-indent-indent-line' and `yaml-indent-indent-region'
+;; respectively in `yaml-mode' hook, eg
+
+;; ```lisp
+;; (defun my-yaml-hook ()
+;;   (setq-local indent-line-function 'yaml-indent-indent-line)
+;;   (setq-local indent-region-function 'yaml-indent-indent-region))
+;; (add-hook 'yaml-mode-hook 'my-yaml-hook)
+;; ```
 
 ;;; Code:
 (defvar yaml-indent-offset 2
